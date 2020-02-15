a3c81aed19e057da163e7bb52b8929b8cd19b7fa
james jomuad <st.james.jomuad@gmail.com>
2020-02-15 18:07:38 +0800

    Render Technologies

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -3,6 +3,7 @@ import './skills.css';
 import axios from 'axios';
 import ProgressCard from '../components/progressCard';
 import Loader from '../components/loader';
+import Parser from 'html-react-parser';
 
 
 export default class Skills extends React.Component {
@@ -64,50 +65,26 @@ export default class Skills extends React.Component {
         <div className="col-md-12 content fadeInUp animated">
 
             {
-                this.getCategories().map((value, index) => {
-                    return (
-                        <>
-                            <h2 key={'h2'+index}>{value}</h2>
-                            {
-                                this.getByCategory(value).map((obj, key) => {
-                                    return <ProgressCard key={key} title={obj.title} score={obj.score}/>
-                                })
-                            }
-                            <div className="spacing-3"></div>
-                        </>
-                    );
-                })
+                this.getCategories().map((value, index) => (
+                    <>
+                        <h2 key={'h2'+index}>{value}</h2>
+                        {
+                            this.getByCategory(value).map((obj, key) => {
+                                return <ProgressCard key={key} title={obj.title} score={obj.score}/>
+                            })
+                        }
+                        <div key={'space'+index} className="spacing-3"></div>
+                    </>
+                ))
             }
-
 
             <h2>Technologies:</h2>
             <div id="logos">
-                <i className="devicon-html5-plain"></i>
-                <i className="devicon-css3-plain"></i>
-                <i className="devicon-javascript-plain"></i>
-                <i className="devicon-vuejs-plain"></i>
-                <i className="devicon-react-original"></i>
-                <i className="devicon-angularjs-plain"></i>
-                <i className="devicon-jquery-plain"></i>
-                <i className="devicon-php-plain"></i>
-                <i className="devicon-nodejs-plain"></i>
-                <i className="devicon-amazonwebservices-original"></i>
-                <i className="devicon-mysql-plain"></i>
-                <i className="devicon-laravel-plain"></i>
-                <i className="devicon-wordpress-plain"></i>
-                <i className="devicon-bootstrap-plain"></i>
-                <i className="devicon-git-plain"></i>
-                <i className="devicon-less-plain-wordmark"></i>
-                <i className="devicon-sass-original"></i>
-                <i className="devicon-linux-plain"></i>
-                <i className="devicon-typescript-plain"></i>
-                <i className="devicon-photoshop-plain"></i>
-                <i className="devicon-illustrator-line"></i>
-                <i className="devicon-apache-line-wordmark"></i>
-                <i className="devicon-php-plain"></i>
-                <i className="devicon-slack-plain"></i>
-                <i className="devicon-symfony-original"></i>
-                <i className="devicon-visualstudio-plain"></i>
+                {
+                    this.state.data.technologies.map((tech, i) => {
+                        return Parser(tech)
+                    })
+                }
             </div>
   
         </div>
